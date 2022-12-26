456229a84676427416b5f13fc045a7ab8fcdf32d
Alonso Casas <alonso.casas@pucp.edu.pe>
2022-12-26 15:25:26 -0500

    C1

---
+++ valores.js
@@ -0,0 +1,17 @@
+/*TIPOS PRIMITIVOS*/
+
+/*Número - Texto - Booleano*/
+40
+
+"Alonso Casas Cuadra" //Comilla dice que generamos String
+
+true
+
+false
+
+null
+undefined           //Valor reservado que genera JS
+
+/*TIPOS OBJETOS */
+[1,2,3]             
+{nombre: "Alonso"} 
