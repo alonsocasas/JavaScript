808f7adeca429fbd0f76efa597d5bba8d71b86fd
Alonso Casas <alonso.casas@pucp.edu.pe>
2022-12-26 16:17:07 -0500

    c2

---
+++ variables.js
@@ -0,0 +1,15 @@
+var nombre = "Alonso"
+var apellido = "Casas"
+var edad = 21
+
+//Declarando
+var altura;
+//Inicializando
+altura = 1.71;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: "David",
+  edad: 21
+}
