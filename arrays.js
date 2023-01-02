3e34ab1f568dd5f07edef29172a6329b64cedaf2
Alonso Casas <alonso.casas@pucp.edu.pe>
2023-01-02 12:16:20 -0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var frutas = ["Manzana", "Platano", "Cereza", "Fresa"];
+console.log(frutas.length);
 
-console.log(frutas);
+//Insertamos al último
+var masFrutas = frutas.push("Uvas");
+//Eliminamos el último
+var ultimo = frutas.pop("Uvas");
+//Isertamos al inicio
+var nuevaLongitud = frutas.unshift("Uvas");
+//Eliminamos el primero
+var borrarFruta = frutas.shift("Uvas");
+//Saber la posición de un elemento
+var posicion = frutas.indexOf("Cereza")
